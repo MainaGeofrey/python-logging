52041a81437e369e529606da72e0210fe507af50
jeff <mainajfry@gmail.com>
2023-03-30 15:37:03 +0300

    inititial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # python-logging
+
+In a Python project, it is easy to track the code progress and debug using print statements and stack trace. However, logging provides additional features of tracking and debugging with timestamps, file name, the line number of code, and more, differentiating between different types of print statements, and even the option to save print statements to a log file or other locations rather than only viewing it on a console or command line!
+
+This article will introduce the components of logging, how to perform logging within a file, and how to use configuration to perform logging across multiple files in a reusable and scalable manner.
+
+https://towardsdatascience.com/basic-to-advanced-logging-with-python-in-10-minutes-631501339650
---
+++ config.py
@@ -0,0 +1,57 @@
+import logging.config
+
+
+class LevelOnlyFilter:
+    def __init__(self, level):
+        self.level = level
+
+    def filter(self, record):
+        return record.levelno == self.level
+
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "loggers": {
+        "": {  # root logger
+            "level": "DEBUG",
+            "propagate": False,
+            "handlers": ["stream_handler", "file_handler"],
+        },
+        "custom_logger": {
+            "level": "DEBUG",
+            "propagate": False,
+            "handlers": ["stream_handler"],
+        },
+    },
+    "handlers": {
+        "stream_handler": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "level": "DEBUG",
+            "filters": ["only_warning"],
+            "formatter": "default_formatter",
+        },
+        "file_handler": {
+            "class": "logging.FileHandler",
+            "filename": "output.log",
+            "mode": "w",
+            "level": "DEBUG",
+            "formatter": "default_formatter",
+        },
+    },
+    "filters": {
+        "only_warning": {
+            "()": LevelOnlyFilter,
+            "level": logging.WARN,
+        },
+    },
+    "formatters": {
+        "default_formatter": {
+            "format": "%(asctime)s-%(levelname)s-%(name)s::%(module)s|%(lineno)s:: %(message)s",
+        },
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+logger.debug("errr")
